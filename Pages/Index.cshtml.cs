3f4f161cd9374e4589424eb58947a6890e1fd12c
Thomas Rayner <thmsrynr@outlook.com>
2021-08-10 15:03:49 -0700

    no container working ex

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CertTester.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string emptyUri = "https://" + HttpContext.Request.Host.ToUriComponent() + "/Empty";
+            X509Certificate2 serverCert = null;
+            var httpClientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (_, cert, __, ___) =>
+                {
+                    serverCert = new X509Certificate2(cert.GetRawCertData());
+                    return true;
+                }
+            };
+            var httpClient = new HttpClient(httpClientHandler);
+            httpClient.Send(new HttpRequestMessage(HttpMethod.Get, emptyUri));
+
+            ViewData["Protocol"] = HttpContext.Request.IsHttps ? "HTTPS" : "HTTP";
+            ViewData["Issuer"] = serverCert.Issuer;
+            ViewData["SubjectName"] = serverCert.SubjectName.Name;
+            ViewData["Serial"] = serverCert.GetSerialNumberString();
+            ViewData["Thumbprint"] = serverCert.Thumbprint;
+            ViewData["Expiration"] = serverCert.NotAfter.ToString();
+        }
+    }
+}
